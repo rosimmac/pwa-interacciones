c75c9633ea3842f14e9b4e570c21d209c5f26d1f
Rosa María Martín Castillo <rosamartn98@gmail.com>
2026-04-13 12:44:12 +0200

    fix: corrección icono pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -23,17 +23,17 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "icono 192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "icono 512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "iconoDefinitivo.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
